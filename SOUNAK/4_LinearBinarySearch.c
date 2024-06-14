7ca540a9d01b9a803a93991117a52d61e83768df
SOUNAK GHOSH <sounak2309@gmail.com>
2024-06-14 09:28:11 +0530

    Linear and Binary Search using array

--- SOUNAK/4_LinearBinarySearch.c
+++ SOUNAK/4_LinearBinarySearch.c
@@ -1,3 +1,5 @@
+// PRACTICE PROGRAM TO UNDERSTAND THE LINEAR AND BINARY SEARCH USING ARRAY
+
 #include <stdio.h>
  
 int linearSearch(int arr[], int size, int element){
@@ -33,14 +35,17 @@ int binarySearch(int arr[], int size, int element){
  
 int main(){
     // Unsorted array for linear search
-    // int arr[] = {1,3,5,56,4,3,23,5,4,54634,56,34};
-    // int size = sizeof(arr)/sizeof(int);
- 
+    int arr1[] = {1,3,5,56,4,3,23,5,4,54634,56,34};
+    int size1 = sizeof(arr1)/sizeof(int);
+    int element1 = 444;
+    int searchIndex1 = binarySearch(arr1, size1, element1);
+    printf("The element %d was found at index %d \n", element1, searchIndex1);
+    
     // Sorted array for binary search
-    int arr[] = {1,3,5,56,64,73,123,225,444};
-    int size = sizeof(arr)/sizeof(int);
-    int element = 444;
-    int searchIndex = binarySearch(arr, size, element);
-    printf("The element %d was found at index %d \n", element, searchIndex);
+    int arr2[] = {1,3,5,56,64,73,123,225,444};
+    int size2 = sizeof(arr2)/sizeof(int);
+    int element2 = 444;
+    int searchIndex2 = binarySearch(arr2, size2, element2);
+    printf("The element %d was found at index %d \n", element2, searchIndex2);
     return 0;
 }
