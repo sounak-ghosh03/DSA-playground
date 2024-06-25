fe5c3b0894e6167fb94a6ab927dae34e694328b0
SOUNAK GHOSH <sounak2309@gmail.com>
2024-06-25 10:00:14 +0530

    Multiple Parenthesis matching

--- SOUNAK/14_MultipleBrackets.c
+++ SOUNAK/14_MultipleBrackets.c
@@ -1,16 +1,16 @@
-// We have to see if the given expression has balanced all the three types of backets which means every opening bracket must have a corresponding 
+// We have to see if the given expression has balanced all the three types of backets which means every opening bracket must have a corresponding
 // closing bracket and vice versa.
 
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 struct stack
 {
     int size;
     int top;
     char *arr;
 };
- 
+
 int isEmpty(struct stack *ptr)
 {
     if (ptr->top == -1)
@@ -22,7 +22,7 @@ int isEmpty(struct stack *ptr)
         return 0;
     }
 }
- 
+
 int isFull(struct stack *ptr)
 {
     if (ptr->top == ptr->size - 1)
@@ -34,87 +34,106 @@ int isFull(struct stack *ptr)
         return 0;
     }
 }
- 
-void push(struct stack* ptr, char val){
-    if(isFull(ptr)){
+
+void push(struct stack *ptr, char val)
+{
+    if (isFull(ptr))
+    {
         printf("Stack Overflow! Cannot push %d to the stack\n", val);
     }
-    else{
+    else
+    {
         ptr->top++;
         ptr->arr[ptr->top] = val;
     }
 }
- 
-char pop(struct stack* ptr){
-    if(isEmpty(ptr)){
+
+char pop(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+    {
         printf("Stack Underflow! Cannot pop from the stack\n");
         return -1;
     }
-    else{
+    else
+    {
         char val = ptr->arr[ptr->top];
         ptr->top--;
         return val;
     }
 }
- 
-char stackTop(struct stack* sp){
+
+char stackTop(struct stack *sp)
+{
     return sp->arr[sp->top];
 }
- 
-int match(char a, char b){
-    if(a=='{' && b=='}'){
+
+int match(char a, char b)
+{
+    if (a == '{' && b == '}')
+    {
         return 1;
     }
-    if(a=='(' && b==')'){
+    if (a == '(' && b == ')')
+    {
         return 1;
     }
-    if(a=='[' && b==']'){
+    if (a == '[' && b == ']')
+    {
         return 1;
     }
-  return 0;  
+    return 0;
 }
- 
-int parenthesisMatch(char * exp){
+
+int parenthesisMatch(char *exp)
+{
     // Create and initialize the stack
-    struct stack* sp;
+    struct stack *sp;
     sp->size = 100;
     sp->top = -1;
     sp->arr = (char *)malloc(sp->size * sizeof(char));
     char popped_ch;
- 
-    for (int i = 0; exp[i]!='\0'; i++)
+
+    for (int i = 0; exp[i] != '\0'; i++)
     {
-        if(exp[i]=='(' || exp[i]=='{' || exp[i]=='['){
+        if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
+        {
             push(sp, exp[i]);
         }
-        else if(exp[i]==')'|| exp[i]=='}' || exp[i]==']'){
-            if(isEmpty(sp)){
+        else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']')
+        {
+            if (isEmpty(sp))
+            {
                 return 0;
             }
             popped_ch = pop(sp);
-            if(!match(popped_ch, exp[i])){ 
-              return 0;  
+            if (!match(popped_ch, exp[i]))
+            {
+                return 0;
             }
         }
     }
- 
-    if(isEmpty(sp)){
+
+    if (isEmpty(sp))
+    {
         return 1;
     }
-    else{
+    else
+    {
         return 0;
     }
-    
 }
- 
+
 int main()
 {
-    char * exp = "[4-6]((8){(9-8)})";
-    
-    if(parenthesisMatch(exp)){
+    char *exp = "[4-6]((8){(9-8)})";
+
+    if (parenthesisMatch(exp))
+    {
         printf("The brackets are balanced");
     }
-    else{
+    else
+    {
         printf("The brackets are not balanced");
     }
     return 0;
