6aff1c174f7485d5876fd3320c3fe01d9cd282ea
SOUNAK GHOSH <sounak2309@gmail.com>
2024-07-09 09:29:51 +0530

    selection sort algorithm

---
+++ SOUNAK/21_SelectionSort.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printArray(int *A, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
+
+void selectionSort(int *A, int n)
+{
+    int indexOfMin, temp;
+    printf("Running Selection sort...\n");
+    for (int i = 0; i < n - 1; i++)
+    {
+        indexOfMin = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (A[j] < A[indexOfMin])
+            {
+                indexOfMin = j;
+            }
+        }
+        // Swap A[i] and A[indexOfMin]
+        temp = A[i];
+        A[i] = A[indexOfMin];
+        A[indexOfMin] = temp;
+    }
+}
+
+    // Input Array (There will be total n-1 passes. 5-1 = 4 in this case!)
+    //  00  01  02  03  04
+    // |03, 05, 02, 13, 12
+
+    // After first pass
+    //  00  01  02  03  04
+    //  02,|05, 03, 13, 12
+
+    // After second pass
+    // 00  01  02  03  04
+    // 02, 03,|05, 13, 12
+
+    // After third pass
+    // 00  01  02  03  04
+    // 02, 03, 05,|13, 12
+
+    // After fourth pass
+    // 00  01  02  03  04
+    // 02, 03, 05, 12,|13
+
+int main()
+{
+    int n;
+    printf("Enter the size of array: \n");
+    scanf("%d", &n);
+    int *A = (int *)malloc(n * sizeof(int));
+    printf("Enter the elements of the array\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+
+    printArray(A, n);
+    selectionSort(A, n);
+    printArray(A, n);
+    free(A);
+    return 0;
+}
